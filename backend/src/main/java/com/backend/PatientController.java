4456f1570a1aa4dc9f4db30837cb7bd2085ddb6a
malickabban <malick.abban@gmail.com>
2024-01-24 00:46:22 -0500

    Backend functions now use java object instead of the proto.

--- backend/src/main/java/com/backend/PatientController.java
+++ backend/src/main/java/com/backend/PatientController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
@@ -17,21 +20,30 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "http://localhost:3000")
 public class PatientController {
 
+    public static void main(String[] args){
+        try {
+            PatientController pat = new PatientController();
+            System.out.println(pat.getPatients());
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
     @Autowired
     private GetPatientsController getPatientsController;
 
     @GetMapping("/patients")
     @ResponseBody
-    public ResponseEntity<List<Protopatient.Patient>> getPatients() {
+    public String getPatients() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
             // Get Proto messages from backend
-            List<Protopatient.Patient> protoPatients = getPatientsController.getData();
-
-            // Send Proto messages to the front end
-            return new ResponseEntity<>(protoPatients, HttpStatus.OK);
+            getPatientsController = new GetPatientsController();
+            List<com.backend.Patient> patients = getPatientsController.getData();
+            return objectMapper.writeValueAsString(patients);
         } catch (IOException e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return "error";
         }
     }
 }
