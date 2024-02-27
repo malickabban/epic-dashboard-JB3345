2352eb4e1255dafe7e309ddccb960e9d8f58af9a
Jtmixon5 <jtmixon10@gmail.com>
2024-02-27 18:09:09 -0500

    Medications Added to Patient
    Could need more testing

--- backend/src/main/java/com/backend/Patient.java
+++ backend/src/main/java/com/backend/Patient.java
@@ -8,6 +8,7 @@ public class Patient {
     public String[] conditions;
     public String[] observations;
     public String[] encounters;
+    public String[] medications;
 
     public Patient(String name){
         this.name = name;
@@ -38,6 +39,9 @@ public class Patient {
     public String[] getEncounters() {
         return encounters;
     }
+    public String[] getMedications() {
+        return medications;
+    }
 
 
     // Setter methods
@@ -52,6 +56,9 @@ public class Patient {
     }
     public void setObservations(String[] observations) {
         this.observations = observations;
+    }
+    public void setMedications(String[] medications) {
+        this.medications = medications;
     }
     public void setGeneralPractitioner(String general_practitioner) {
         this.general_practitioner = general_practitioner;
